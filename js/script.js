c4b53cfa65070424759b23c90aa3be25abff9c77
dgonzalez777 <1989.daniela@gmail.com>
2023-04-11 14:38:07 -0400

    Add linters js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn"); 
+const navbar = document.querySelector('.navbar');
+const menuBtn = document.querySelector('.menu-btn');
 
-menuBtn.addEventListener ("click", (e)=> {
-  navbar.classList.toggle("navbar-active");  
-  menuBtn.firstElementChild.classList.toggle("none");
-  menuBtn.lastElementChild.classList.toggle("none");
-})
+menuBtn.addEventListener('click', () => {
+  navbar.classList.toggle('navbar-active');
+  menuBtn.firstElementChild.classList.toggle('none');
+  menuBtn.lastElementChild.classList.toggle('none');
+});
