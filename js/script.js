8fd3248d41f0775a17ea93addf142e132f6ce7a0
dgonzalez777 <1989.daniela@gmail.com>
2023-04-11 14:40:45 -0400

    test3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector('.navbar');
 const menuBtn = document.querySelector('.menu-btn');
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener ('click', () => {
   navbar.classList.toggle('navbar-active');
   menuBtn.firstElementChild.classList.toggle('none');
   menuBtn.lastElementChild.classList.toggle('none');
